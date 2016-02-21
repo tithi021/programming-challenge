7dbe61e6052ddc98134bd9a715f29eced68563e8
tithi021 <afroza021@gmail.com>
2016-02-21 13:21:40 +0600

    complete_all_task

--- api/app.js
+++ api/app.js
@@ -8,17 +8,30 @@ app.options('*', cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+//conversion
+//called API
+//celsius to fahrenheit conversion
 app.post('/api/temperature/celsius/to/fahrenheit', function (req, res) {
   var celsius = req.body.celsius;
-  var fahrenheit = celsius * 9 / 5 + 32;
+  var fahrenheit = celsius * 9 / 5 + 32; 
   res.json({ 'fahrenheit' : fahrenheit });
 });
 
-app.post('/api/temperature/fahrenheit/to/celsius', function (req, res) {
+//fahrenheit to celsius conversion
+app.post('/api/temperature/fahrenheit/to/celcius', function (req, res) {
   var fahrenheit = req.body.fahrenheit;
-  var celsius = fahrenheit - 32 * 5 / 9;
+  var celsius = fahrenheit - 32 * 5 / 9; 
   res.json({ 'celsius' : celsius });
 });
+
+//kelvin to celsius and fahrenheit conversion
+app.post('/api/temperature/kelvin/to/celsiusfahrenheit', function (req, res) {
+  var kelvin = req.body.kelvin;
+  var celsius = kelvin - 273.15; //formula for convertion kelvin to celsius
+  var fahrenheit = kelvin * 9 / 5 - 459.67; //formula for conversion kelvin to fahrenheit
+  res.json({ 'celsius' : celsius, 'fahrenheit' : fahrenheit }); 
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
